a33853004c59e502023f586c1ad0ed0560912114
TURMA-A <henriquepromail9863@gmail.com>
2024-04-09 09:25:42 -0300

    update exer11

--- exercicio11.py
+++ exercicio11.py
@@ -3,4 +3,21 @@ entrada2 = 14:20
 saida = 6:05'''
 
 hora1 = int(input("Digite a primeira hora: "))
+minutos1 = int(input("Digite os primeiros minutos: "))
 hora2 = int(input("Digite a segunda hora: "))
+minutos2 = int(input("Digite os segundos minutos: "))
+
+horasaida = hora1 + hora2
+minutosaida = minutos1 + minutos2
+
+if minutosaida >= 60:
+    horasaida = horasaida ++ 1
+    minutosaida = minutosaida - 60
+if horasaida >= 24:
+    horasaida = horasaida - 24
+elif horasaida > 12 and horasaida < 24:
+    horasaida = horasaida - 12
+if horasaida > 12:
+    horasaida = horasaida - 12
+
+print(f'{horasaida}:{minutosaida}')
