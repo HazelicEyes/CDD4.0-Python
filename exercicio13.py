b44c11ee29bee0ccad596e332667b07aeddeafef
TURMA-A <henriquepromail9863@gmail.com>
2024-04-09 11:50:12 -0300

    add exer14

--- exercicio13.py
+++ exercicio13.py
@@ -1,5 +1,5 @@
 soma = 0
-for x in range (10):
+for contador in range (10):
     n = int(input("Insira um numero: "))
     soma = soma + n
 print(f'Valor da soma: {soma}')
---
+++ exercicio14.py
@@ -0,0 +1,4 @@
+n1 = int(input("Digite um numero inteiro: "))
+for x in range (1,11):
+    multi = n1 * x
+    print(f'{n1}x{x}={multi}')
