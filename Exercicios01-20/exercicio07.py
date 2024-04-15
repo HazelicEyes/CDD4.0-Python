f8d56140b5d59837ec4509e4cc6b9f9d831ec904
TURMA-A <henriquepromail9863@gmail.com>
2024-04-15 09:31:59 -0300

    update exer07

--- Exercicios01-20/exercicio07.py
+++ Exercicios01-20/exercicio07.py
@@ -1,12 +1,12 @@
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 n3 = float(input("Digite a terceira nota: "))
-media = (n1 + n2 + n3) / 2
+n4 = float(input("Digite a quarta nota: "))
+media = (n1 + n2 + n3 + n4) / 4
 
 if media >= 7:
     print("Media:", media, "\nAluno aprovado")
-elif media < 7 and media >= 4:
+elif media < 7 and media >= 5:
     print("Media:", media, "\nAluno em recuperação")
 else:
     print("Media:", media, "\nAluno reprovado")
-
