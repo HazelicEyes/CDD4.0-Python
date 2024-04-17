07d2b81b0890468080b1657620fd18b4ef3ca801
TURMA-A <henriquepromail9863@gmail.com>
2024-04-17 11:52:17 -0300

    misc

---
+++ exercicio29.py
@@ -0,0 +1,15 @@
+notas = ['','','','','']
+soma = 0
+contador = 0
+for x in range(len(notas)):
+    notas[x] = float(input("Digite as notas dos alunos: "))
+
+for y in range(len(notas)):
+    soma += notas[y]
+media = soma / 5
+
+for z in range(len(notas)):
+    if notas[z] > media:
+        contador += 1
+print(f'A media da sala é {media}')
+print(f'A quantidade de alunos que tiveram a nota maior que a media é {contador}')
