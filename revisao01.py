4c889487415e4ba49d6958199550eadc7674e83b
TURMA-A <henriquepromail9863@gmail.com>
2024-04-23 10:56:53 -0300

    add revisions

---
+++ revisao01.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+soma = nota1 + nota2
+media = soma / 2
+print(f'A media é {media}.')
---
+++ revisao05.py
@@ -0,0 +1,7 @@
+ano = 2024
+idade = int(input("Digite a idade: "))
+aniversario = input("Voce ja fez aniversario esse ano? (s/n): ")
+if aniversario == 'n' or aniversario == 'n':
+    ano = ano -1
+nascimento = ano - idade
+print(f'A pessoa nasceu em {nascimento}')
