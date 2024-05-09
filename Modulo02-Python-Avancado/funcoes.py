25937327b16a4f7798ffe3582a8eae97a342c57a
TURMA-A <henriquepromail9863@gmail.com>
2024-05-09 11:12:38 -0300

    more exercices

--- Modulo02-Python-Avancado/funcoes.py
+++ Modulo02-Python-Avancado/funcoes.py
@@ -1,14 +1,30 @@
 def imprime_nome(nome):
     print(f'Nome: {nome}')
-
 def piramide(num):
     for x in range(1,num+1):
         for y in range(1,x+1):
             print(x, end=" ")
         print()
-
 def piramide2(num):
     for x in range(1,num+1):
         for y in range(1,x+1):
             print(y, end=" ")
         print()
+def contar_vogais(texto):
+    contador = 0
+    vogais = 'aeiouAEIOU'
+    for x in texto:
+        if x in "aeiouAEIOU":
+            contador += 1
+    print(contador)
+def estoque(produto, quantidade, valor):
+    total = valor*quantidade
+    return total
+
+def valor(n):
+    if n > 0:
+        return "Positivo"
+    elif n < 0 :
+        return 'Negativo'
+    else:
+        return 'Zero'
--- Modulo02-Python-Avancado/main.py
+++ Modulo02-Python-Avancado/main.py
@@ -1,5 +1,22 @@
 from funcoes import *
 
-piramide(10)
-
+'''piramide(10)
 piramide2(10)
+x = int(input("Digite um numero:))
+piramide2(x)'''
+
+'''texto = input("Digite alguma coisa que eu vou contar as vogais :D: ")
+contar_vogais(texto)'''
+
+'''n = input("Digite o nome do produto:")
+q = int(input("Digite a quantidade do produto:"))
+v = float(input("Digite o valor unitario do produto:"))
+resposta = estoque(n,q,v)
+soma = 0
+soma += resposta
+print(f'Voce tem {resposta} reais de {n} no estoque')
+print(soma)'''
+
+numero = int(input("Digite um numero: "))
+resposta = valor(numero)
+print(f'O numero {numero} é {resposta}')
