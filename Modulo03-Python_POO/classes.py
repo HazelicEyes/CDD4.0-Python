fe422b6205d0cf77bb687756e02d793d025b55e7
HazelicEyes <henriquepromail9863@gmail.com>
2024-05-17 00:33:32 -0300

    testes

--- Modulo03-Python_POO/classes.py
+++ Modulo03-Python_POO/classes.py
@@ -5,16 +5,22 @@ class Pessoa():
         self.peso = pesoaluno #esses 3 são atributos #atributos usam sempre self, atributos atribuem valores
         self.idade = idadealuno
     def falar(self, falar):
+        flagFalar = true
         print(f'{self.nome} esta falando')
     def pararDeFalar(self, falar):
+        flagFalar = false
         print(f'{self.nome} parou de falar')
 
     def comer(self, comida, bebida):
+        flagComer = true
         print(f'{self.nome} foi comer {comida} e beber {bebida}')
     def pararDeComer(self, comida, bebida):
+        flagComer = false
         print(f'{self.nome} parou de comer')
 
     def dormir(self, dormir):
+        flagDormir = true
         print(f'{self.nome} esta dormindo')
     def acordar(self, acordar):
+        flagDormir = false
         print(f'{self.nome} acordou.')
