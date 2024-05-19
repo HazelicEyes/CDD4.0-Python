aeb9d37af6be87cb6b6b52b3163c40f295315cdc
HazelicEyes <henriquepromail9863@gmail.com>
2024-05-19 00:39:05 -0300

    finalizei o primeiro desafio

--- Modulo03-Python_POO/classes.py
+++ Modulo03-Python_POO/classes.py
@@ -1,26 +1,39 @@
 class Pessoa():
 # Classes tem Metodos e Atributos, (Metodos sao Funcoes e Atributos são Variaveis)
-    def __init__(self, nomealuno, pesoaluno, idadealuno, comendo=False):
-        self.nome = nomealuno
-        self.peso = pesoaluno #esses 3 são atributos #atributos usam sempre self, atributos atribuem valores
-        self.idade = idadealuno
+    def __init__(self, nome, peso, idade):
+        self.nome = nome
+        self.peso = peso #esses 3 são atributos #atributos usam sempre self, atributos atribuem valores
+        self.idade = idade
+        self.acao = "nenhum"
     def falar(self, falar):
-        flagFalar = true
-        print(f'{self.nome} esta falando')
-    def pararDeFalar(self, falar):
-        flagFalar = false
-        print(f'{self.nome} parou de falar')
+        if self.acao == "nenhum":
+            self.acao = "falando"
+            print(f'{self.nome} esta falando <{falar}>')
+        else:
+            print(f'{self.nome} não pode falar <{falar}> porque esta <{self.acao}>')
+    def pararDeFalar(self):
+        if self.acao == "falando":
+            self.acao = "nenhum"
+            print(f'{self.nome} parou de falar')
 
-    def comer(self, comida, bebida):
-        flagComer = true
-        print(f'{self.nome} foi comer {comida} e beber {bebida}')
-    def pararDeComer(self, comida, bebida):
-        flagComer = false
-        print(f'{self.nome} parou de comer')
+    def comer(self, comida):
+        if self.acao == "nenhum":
+            self.acao = "comendo"
+            print(f'{self.nome} esta comendo <{comida}>')
+        else:
+            print(f'{self.nome} não pode comer <{comida}> porque esta <{self.acao}>')
+    def pararDeComer(self):
+        if self.acao == "comendo":
+            self.acao = "nenhum"
+            print(f'{self.nome} parou de comer')
 
-    def dormir(self, dormir):
-        flagDormir = true
-        print(f'{self.nome} esta dormindo')
-    def acordar(self, acordar):
-        flagDormir = false
-        print(f'{self.nome} acordou.')
+    def dormir(self):
+        if self.acao == "nenhum":
+            self.acao = "dormindo"
+            print(f'{self.nome} foi <dormir>.')
+        else:
+            print(f'{self.nome} não pode <dormir> porque esta <{self.acao}>')
+    def acordar(self):
+        if self.acao == "dormindo":
+            self.acao = "nenhum"
+            print(f'{self.nome} <acordou>')
