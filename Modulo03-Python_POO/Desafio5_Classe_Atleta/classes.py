a26d1c63801426c7630be88a0a62083ecc4819a7
HazelicEyes <henriquepromail9863@gmail.com>
2024-05-23 03:27:59 -0300

    finalizei o desafio atleta

--- Modulo03-Python_POO/Desafio5_Classe_Atleta/classes.py
+++ Modulo03-Python_POO/Desafio5_Classe_Atleta/classes.py
@@ -1,35 +1,56 @@
 class Atleta:
     def __init__(self):
         self.aposentado = False
+        self.aquecido = False
         self.peso = 0
 
     def aposentar(self):
-        if self.aposentado == False:
+        if self.aposentado is False:
             self.aposentado = True
             print(f'O atleta foi APOSENTADO. Bom descanso.')
         else:
             print(f'O atleta ja esta APOSENTADO.')
 
     def aquecer(self):
-        print(f' O atleta esta AQUECENDO.')
+        if self.aquecido is False:
+            self.aquecido = True
+            print(f' O atleta esta AQUECENDO.')
 
 class Corredor(Atleta):
     def __init__(self):
         super().__init__()
     def correr(self):
-        print(f' O atleta esta correndo')
+        if self.aposentado is False:
+            if self.aquecido is True:
+                print(f' O atleta esta CORRENDO')
+            else:
+                print(f' O atleta não pode correr porque não aqueceu.')
+        else:
+            print(f' O atleta não pode correr porque esta aposentado.')
 
 class Nadador(Atleta):
     def __init__(self):
         super().__init__()
     def nadar(self):
-        print(f' O atleta esta nadando')
+        if self.aposentado is False:
+            if self.aquecido is True:
+                print(f' O atleta esta NADANDO')
+            else:
+                print(f' O atleta não pode NADAR porque não aqueceu.')
+        else:
+            print(f' O atleta não pode NADAR porque esta aposentado.')
 
 class Ciclista(Atleta):
     def __init__(self):
         super().__init__()
     def pedalar(self):
-        print(f' O atleta esta pedalando')
+        if self.aposentado is False:
+            if self.aquecido is True:
+                print(f' O atleta esta PEDALANDO')
+            else:
+                print(f' O atleta não pode PEDALAR porque não aqueceu.')
+        else:
+            print(f' O atleta não pode PEDALAR porque esta aposentado.')
 
 class TriAtleta(Corredor, Nadador, Ciclista):
     def __init__(self):
--- Modulo03-Python_POO/Desafio5_Classe_Atleta/main.py
+++ Modulo03-Python_POO/Desafio5_Classe_Atleta/main.py
@@ -1,1 +1,7 @@
 from classes import *
+
+a1 = TriAtleta()
+a1.aquecer()
+a1.correr()
+a1.nadar()
+a1.pedalar()
