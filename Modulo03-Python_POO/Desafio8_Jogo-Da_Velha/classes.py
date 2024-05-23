fa124dc8a1279a81393976fa618e967d4b2275e6
TURMA-A <henriquepromail9863@gmail.com>
2024-05-23 10:45:18 -0300

    começa desafio de jogo da velha

---
+++ Modulo03-Python_POO/Desafio8_Jogo-Da_Velha/classes.py
@@ -0,0 +1,6 @@
+import random
+class Jogo:
+    def __init__(self):
+        self.tabuleiro = [['A1','B1','C1'],
+                          ['A2','B2','C2'],
+                          ['A3','B2','C3']]
